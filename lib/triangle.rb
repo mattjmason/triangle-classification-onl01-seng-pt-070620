b2fce8e6f99344ab6d31c250ddf56d56ea0a4b23
Matt Mason <masonmj10@gmail.com>
2020-08-17 06:50:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,41 +1,31 @@
 class Triangle
   # write code here
-   def initialize(s1, s2, s3)
+    attr_accessor
 
-  
-    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
-      raise TriangleError
-    end
-
-    if s1 <= 0 || s2 <= 0 ||s3 <= 0
-      raise TriangleError
-    end
-
-    if s1 == nil || s2 == nil ||s3 == nil
-      raise TriangleError
-    end
-
-    @s1 = s1
-    @s2 = s2
-    @s3 = s3
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
   end
 
   def kind
-    if @s1 == @s2 && @s1 == @s3
-      return :equilateral
-    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
-      return :isosceles
-    else
-      return :scalene
+    if @length1 <= 0 || @length2 <= 0 || @length3 <= 0
+      begin
+        raise TriangleError
+      end
+    elsif @length1 + @length2 <= @length3 || @length1 + @length3 <= @length2 || @length2 + @length3 <= @length1
+      begin
+        raise TriangleError
+      end
+    elsif @length1 == @length2 && @length2 == @length3
+        :equilateral
+    elsif @length1 == @length2 || @length2 == @length3 || @length1 == @length3
+        :isosceles
+    else @length1 != @length2 || @length2 != @length3 || @length1 != @length3
+        :scalene
     end
   end
 
-end
-
-
-
-class TriangleError < StandardError
-  def message
-    "This is not a valid triangle."
+  class TriangleError < StandardError
   end
 end
